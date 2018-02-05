f1d98059e27ce4e53b1b2e2796e5537ad66b8e27
mmcknett <mmcknett@godaddy.com>
2018-02-05 09:45:41 -0800

    Working sample that prints to the command line

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace sample
+{
+    class SampleProgram
+    {
+        static void Main(string[] args)
+        {
+            Writer.Write();
+
+            // Pretend we got these things in as args...
+            args = new string[]{ "First Thing", "Second Thing" };
+
+            var arg0Writer = new InstanceWriter(args[0]);
+            var arg1Writer = new InstanceWriter(args[1]);
+
+            arg0Writer.Write();
+            arg1Writer.Write();
+        }
+    }
+
+    class Writer
+    {
+        public static void Write()
+        {
+            Console.WriteLine(WhatToSay());
+        }
+
+        private static string WhatToSay()
+        {
+            return "Hello Amy!";
+        }
+    }
+
+    class InstanceWriter
+    {
+        private string _thingToWrite;
+
+        public InstanceWriter(string thingToWrite)
+        {
+            this._thingToWrite = thingToWrite;
+        }
+
+        public void Write()
+        {
+            Console.WriteLine(_thingToWrite);
+        }
+    }
+}
